9e08321836a3a4e57725735d327a5418c7cccb1e
DefinitelyNotAnAssassin <dharklike@gmail.com>
2025-01-01 14:00:00 +0800

    Settings Configuration

--- EnrollmentSystem/settings.py
+++ EnrollmentSystem/settings.py
@@ -155,3 +155,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
